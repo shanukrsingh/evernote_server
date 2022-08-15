b083840ec27727876e5cdbca33be78f077e11821
shanukrsingh <56463680+shanukrsingh@users.noreply.github.com>
2022-08-15 16:24:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const express = require('express');
+const app = express();
+let bodyParser = require('body-parser');
+var cors = require('cors');
 const port = process.env.port;
 
 app.use(cors());
 
-// support parsing of application/json type post data
 app.use(bodyParser.json());
 
-//support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded(({ extended: true })));
 
 require(`${__dirname}/app/routes`)(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at ${port}`)
-})
+    console.log(`server app listening at ${port}`)
+});
